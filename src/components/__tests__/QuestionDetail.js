ee0d9ba4efa519b0c902d24a8d12327fabd0d814
Fernando Brandao <fernando@fernando.local>
2018-11-09 15:29:36 +0000

    added notificationViewer

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,5 +1,39 @@
-describe('The question Detail component', () => {
-    it('Should not regress', () => {
+import { mapStateToProps, QuestionDetailDisplay} from '../QuestionDetail';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
+describe('The question Detail component', () => {
+    describe('The container Element', ()=> {
+        describe('mapStateToProps', () => {
+            it('should map the state props correctly', ()=> {
+                const sampleQuestion = {
+                    question_id:42,
+                    body:'space is big'
+                };
+
+                const appState = {
+                    questions:[sampleQuestion]
+                };
+                const ownprops = {
+                    question_id:42,
+                };
+                const componentState = mapStateToProps(appState, ownprops);
+                console.log(componentState);
+            });
+        });
+    });
+
+    describe('the Display element', () => {
+        it('Should not regress', () => {
+            const tree = renderer.create(
+                <QuestionDetailDisplay
+                title="tilte"
+                body='43'
+                answer_count={0}
+                tags={['hitchhicking']}
+                />
+            );
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
     });
 });
