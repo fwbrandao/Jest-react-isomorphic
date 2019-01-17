4efb47c81f2b12c0877c29f5406574829d2a446b
Fernando Brandao <fernando.brandao@adarga.ai>
2019-01-17 16:30:17 +0000

    test fix v2

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,14 +1,14 @@
 import { handleFetchQuestion } from './fetch-question-saga';
 import fetch from 'isomorphic-fetch';
 
-describe('Fetch question saga', () => {
+describe('Fetch questios saga', () => {
     beforeAll(() => {
-        fetch.__setValue([{quetion_id:42}]);
+        fetch.__setValue([{question_id: 42}]);
     });
-    it ('should fetch the saga', async () => {
-        const gen = handleFetchQuestion({question_id:42});
+    it('Should fetch the saga', async () => {
+        const gen = handleFetchQuestion({question_id: 42});
         const { value } = await gen.next();
-        expect(value).toEqual([{question_id:42}]);
-        expect(fetch).toHaveBeenCalledWith('/api/questions/42')
+        expect(value).toEqual([{question_id: 42}]);
+        expect(fetch).toHaveBeenCalledWith(`/api/questions/42`);
     });
 });
